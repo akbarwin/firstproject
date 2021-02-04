951f3bf0ddf924a1a98cbb461183345c24289d69
Akbar <akemelov@gmail.com>
2021-02-04 10:43:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// alert("hello,Sir");
+
+// let balls = 5;
+// while (balls>0) {
+//     document.write  ('another ball,right now <br>');
+//     balls=balls-1;
+// }
+// document.write('life without ball isn"t same');
+// let balls = 4;
+// if (balls<3) {
+// alert('we need a baaaall');
+
+// } else {
+//     document.write('ooh, everything is okay');
+// }
+
+
+
+
+alert('welcome,mr.Akbar');
+
+let i = 0;
+while ( i<2 ) {
+    document.write('Happy birthay to you <br>');
+    i=i+1;
+}
+let nam = 'Joe';
+document.write('Happy birthday dear ' + nam + '<br>');
+document.write('Happy birthday to you');
+
+
