a6d24066ef44e625e2916bdbf97f49c1d66763f9
Akbar <akemelov@gmail.com>
2021-02-05 17:46:33 +0600

    new

--- js/script.js
+++ js/script.js
@@ -1,31 +1,19 @@
-// alert("hello,Sir");
-
-// let balls = 5;
-// while (balls>0) {
-//     document.write  ('another ball,right now <br>');
-//     balls=balls-1;
-// }
-// document.write('life without ball isn"t same');
-// let balls = 4;
-// if (balls<3) {
-// alert('we need a baaaall');
-
-// } else {
-//     document.write('ooh, everything is okay');
-// }
+const NumberofFilms = +prompt( "сколько фильмов вы посмотрели?", "");
+ const personalMovieDB = {
+    count: NumberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+ };
 
 
+ const a = prompt('один из последних просмотренных вами фильмов?', ''),
+       b = prompt('на сколько оцените его?'),
+       c = prompt('один из последних просмотренных вами фильмов?', ''),
+       d = prompt('на сколько оцените его?');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-alert('welcome,mr.Akbar');
-
-let i = 0;
-while ( i<2 ) {
-    document.write('Happy birthay to you <br>');
-    i=i+1;
-}
-let nam = 'Joe';
-document.write('Happy birthday dear ' + nam + '<br>');
-document.write('Happy birthday to you');
-
-
+console.log(personalMovieDB);
